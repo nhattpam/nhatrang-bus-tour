00ff0051b1fb741d8385fd34a8b11015b257fc5c
Nhat Pham <nhat2up@gmail.com>
2023-05-30 08:54:52 +0700

    admin

--- nha-trang-bus-tour-ui/src/App.js
+++ nha-trang-bus-tour-ui/src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
 import { Route, Routes } from 'react-router-dom';
-import Main from './components/Main';
-import Navbar from './components/Navbar';
+import SideBar from './components/SideBar';
+
 function App() {
   return (
+    // <>
+    //   <Routes>
+    //     <Route path='/' element={<Main/>}></Route>
+    //     <Route path='/admin' element={<Navbar />}></Route>
+    //   </Routes>
+    // </>
     <>
-      <Routes>
-        <Route path='/' element={<Main/>}></Route>
-        <Route path='/admin' element={<Navbar />}></Route>
-      </Routes>
+     <Header />
+     <Home />
+     <SideBar />
+     <Footer />
+     
+
     </>
+
     
   );
 }
