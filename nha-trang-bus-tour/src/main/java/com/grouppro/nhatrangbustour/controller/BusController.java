cf6da7beb92a34651c509020152aa126496e32f2
Nhat Pham <nhat2up@gmail.com>
2023-06-06 19:07:08 +0700

    axios

--- nha-trang-bus-tour/src/main/java/com/grouppro/nhatrangbustour/controller/BusController.java
+++ nha-trang-bus-tour/src/main/java/com/grouppro/nhatrangbustour/controller/BusController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://nha-trang-bus-tour.web.app")
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "buses")
