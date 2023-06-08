055b2dd3e83b2783390979341e47f149b82b9588
Nhat Pham <nhat2up@gmail.com>
2023-06-08 08:43:22 +0700

    azure

--- nha-trang-bus-tour-ui/src/services/bus.service.js
+++ nha-trang-bus-tour-ui/src/services/bus.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_API_URL = "http://nhatrangbustourbackend-env.eba-ksxs24vb.ap-northeast-1.elasticbeanstalk.com/api";
+const BASE_API_URL = "https://nhatrangbustourbackend.azurewebsites.net/api"
 // const BASE_API_URL = "https://api.sampleapis.com/beers";
 class BusService{
     
