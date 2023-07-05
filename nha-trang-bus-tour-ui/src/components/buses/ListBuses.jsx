edf46774afccd22ea7b74939af44547c49515eca
Nhat Pham <nhat2up@gmail.com>
2023-07-05 09:06:39 +0700

    pagination

--- nha-trang-bus-tour-ui/src/components/buses/ListBuses.jsx
+++ nha-trang-bus-tour-ui/src/components/buses/ListBuses.jsx
@@ -1,132 +1,140 @@
-import React, { useEffect, useState } from 'react'
-import Header from "../Header";
-import SideBar from "../SideBar";
+import React, { useEffect, useState } from 'react';
+import Header from '../Header';
+import SideBar from '../SideBar';
 import busService from '../../services/bus.service';
 import { Link } from 'react-router-dom';
-
+import ReactPaginate from 'react-paginate';
 
 const ListBuses = () => {
-    const [busList, setBusList] = useState([]);
-    const [msg, setMsg] = useState('');
-    const [searchTerm, setSearchTerm] = useState('');
+  const [busList, setBusList] = useState([]);
+  const [msg, setMsg] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(0);
+  const [busesPerPage] = useState(5);
 
-    useEffect(() => {
-        busService.getAllBuses()
-            .then((res) => {
-                console.log(res.data);
-                setBusList(res.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
+  useEffect(() => {
+    busService
+      .getAllBuses()
+      .then((res) => {
+        console.log(res.data);
+        setBusList(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    const handleSearch = (event) => {
-        setSearchTerm(event.target.value);
-    };
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-    const filteredBuses = busList.filter((bus) => {
-        return (
-            bus.busId.toString().toLowerCase().includes(searchTerm.toLowerCase()) ||
-            bus.busNumber.toString().toLowerCase().includes(searchTerm.toLowerCase()) ||
-            bus.seat.toString().toLowerCase().includes(searchTerm.toLowerCase())
-        );
-    });
-
-    // const deleteBus = (busId) => {
-    //     busService.deleteBus(busId).then((res) => {
-    //         setMsg("Delete bus successfully!");
-    //     }).catch((error) => {
-    //         console.log(error);
-    //     });
-    // };
-
+  const filteredBuses = busList.filter((bus) => {
     return (
-        <>
-            {/* Page Wrapper */}
+      bus.busId.toString().toLowerCase().includes(searchTerm.toLowerCase()) ||
+      bus.busNumber.toString().toLowerCase().includes(searchTerm.toLowerCase()) ||
+      bus.seat.toString().toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
 
-            <div id="wrapper">
-                <SideBar />
-                {/* Content Wrapper */}
+  const pageCount = Math.ceil(filteredBuses.length / busesPerPage);
 
-                <div id="content-wrapper" class="d-flex flex-column">
-                    {/* Main Content */}
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected);
+  };
 
-                    <div id="content">
-                        <Header />
-                        <div class="container-fluid">
+  const offset = currentPage * busesPerPage;
+  const currentBuses = filteredBuses.slice(offset, offset + busesPerPage);
 
-                            {/* Page Heading */}
+  return (
+    <>
+      {/* Page Wrapper */}
+      <div id="wrapper">
+        <SideBar />
+        {/* Content Wrapper */}
+        <div id="content-wrapper" className="d-flex flex-column">
+          {/* Main Content */}
+          <div id="content">
+            <Header />
+            <div className="container-fluid">
+              {/* Page Heading */}
+              <h1 className="h3 mb-2 text-gray-800">List Buses</h1>
 
-                            <h1 class="h3 mb-2 text-gray-800">List Buses</h1>
+              {msg && <p className="text-center text-success">{msg}</p>}
 
-                            {
-                                msg && <p className='text-center text-success'>{msg}</p>
-                            }
-
-                            {/* Search Input */}
-                            <div className="mb-3">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Search Tickets"
-                                    value={searchTerm}
-                                    onChange={handleSearch}
-                                />
-                            </div>
-                            {/* DataTales Example */}
-
-                            <div class="card shadow mb-4">
-                                <div class="card-body">
-                                    <div class="table-responsive">
-                                        <table class="table table-bordered" id="" width="100%" cellspacing="0">
-                                            <thead>
-                                                <tr>
-                                                    <th>Bus Id</th>
-                                                    <th>Bus Number</th>
-                                                    <th>Seat</th>
-                                                    <th>Action</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {
-
-                                                    filteredBuses.map((bus) => (
-                                                        <tr key={bus.busId}>
-                                                            <td key={bus.busId}>{bus.busId}</td>
-                                                            <td key={bus.busNumber}>{bus.busNumber}</td>
-                                                            <td key={bus.seat}>{bus.seat}</td>
-                                                            <td>
-                                                                <div className="btn-group" role="group">
-                                                                    <Link className="btn btn-primary" to={"/edit_bus/" + bus.busId}>
-                                                                        Edit
-                                                                    </Link>
-                                                                    {/* <a className="btn btn-danger" onClick={() => deleteBus(e.busId)}>
-                                                                        Delete
-                                                                    </a> */}
-                                                                </div>
-                                                            </td>
-
-                                                        </tr>
-                                                    ))
-
-                                                }
-
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        {/* /.container-fluid */}
-
-                    </div>
+              {/* Search Input */}
+              <div className="mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Search Tickets"
+                  value={searchTerm}
+                  onChange={handleSearch}
+                />
+              </div>
+              {/* DataTales Example */}
+              <div className="card shadow mb-4">
+                <div className="card-body">
+                  <div className="table-responsive">
+                    <table className="table table-bordered" id="" width="100%" cellSpacing="0">
+                      <thead>
+                        <tr>
+                          <th>Bus Id</th>
+                          <th>Bus Number</th>
+                          <th>Seat</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {currentBuses.map((bus) => (
+                          <tr key={bus.busId}>
+                            <td>{bus.busId}</td>
+                            <td>{bus.busNumber}</td>
+                            <td>{bus.seat}</td>
+                            <td>
+                              <div className="btn-group" role="group">
+                                <Link className="btn btn-primary" to={`/edit_bus/${bus.busId}`}>
+                                  Edit
+                                </Link>
+                                {/* <a className="btn btn-danger" onClick={() => deleteBus(e.busId)}>
+                                  Delete
+                                </a> */}
+                              </div>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
+              </div>
+
+              {/* Pagination */}
+              <ReactPaginate
+                previousLabel={'Previous'}
+                nextLabel={'Next'}
+                breakLabel={'...'}
+                breakClassName={'page-item'}
+                breakLinkClassName={'page-link'}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={'pagination'}
+                activeClassName={'active'}
+                previousClassName={'page-item'}
+                nextClassName={'page-item'}
+                pageClassName={'page-item'}
+                previousLinkClassName={'page-link'}
+                nextLinkClassName={'page-link'}
+                pageLinkClassName={'page-link'}
+              />
             </div>
+            {/* /.container-fluid */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default ListBuses
+export default ListBuses;
--- nha-trang-bus-tour-ui/src/components/orders/ListOrders.jsx
+++ nha-trang-bus-tour-ui/src/components/orders/ListOrders.jsx
@@ -1,23 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import Header from "../Header";
-import SideBar from "../SideBar";
+import React, { useEffect, useState } from 'react';
+import Header from '../Header';
+import SideBar from '../SideBar';
 import orderService from '../../services/order.service';
 import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 
 const ListOrders = () => {
-
   const [orderList, setOrderList] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(0);
+  const [ordersPerPage] = useState(5);
 
   useEffect(() => {
-    orderService.getAllOrders().then((res) => {
-      console.log(res.data);
-
-      setOrderList(res.data);
-    }).catch((error) => {
-      console.log(error);
-      console.log('lol');
-    });
+    orderService
+      .getAllOrders()
+      .then((res) => {
+        console.log(res.data);
+        setOrderList(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   const handleSearch = (event) => {
@@ -33,24 +36,28 @@ const ListOrders = () => {
     );
   });
 
+  const pageCount = Math.ceil(filteredOrders.length / ordersPerPage);
+
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected);
+  };
+
+  const offset = currentPage * ordersPerPage;
+  const currentOrders = filteredOrders.slice(offset, offset + ordersPerPage);
+
   return (
     <>
       {/* Page Wrapper */}
-
       <div id="wrapper">
         <SideBar />
         {/* Content Wrapper */}
-
-        <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content-wrapper" className="d-flex flex-column">
           {/* Main Content */}
-
           <div id="content">
             <Header />
-            <div class="container-fluid">
-
+            <div className="container-fluid">
               {/* Page Heading */}
-
-              <h1 class="h3 mb-2 text-gray-800">List Orders</h1>
+              <h1 className="h3 mb-2 text-gray-800">List Orders</h1>
 
               {/* Search Input */}
               <div className="mb-3">
@@ -62,52 +69,60 @@ const ListOrders = () => {
                   onChange={handleSearch}
                 />
               </div>
-
               {/* DataTales Example */}
-
-              <div class="card shadow mb-4">
-                <div class="card-body">
-                  <div class="table-responsive">
-                    <table class="table table-bordered" id="" width="100%" cellspacing="0">
+              <div className="card shadow mb-4">
+                <div className="card-body">
+                  <div className="table-responsive">
+                    <table className="table table-bordered" id="" width="100%" cellSpacing="0">
                       <thead>
                         <tr>
                           <th>Order Id</th>
                           <th>Order Date</th>
                           <th>Payment Method</th>
                           <th>User Id</th>
-
                         </tr>
                       </thead>
                       <tbody>
-                        {
-
-                          filteredOrders.map((order) => (
-                            <tr key={order.orderId}>
-                              <td>{order.orderId}</td>
-                              <td>{order.orderDate}</td>
-                              <td>{order.payment.paymentMethod}</td>
-                              <td>{order.userId}</td>
-                            </tr>
-                          ))
-
-                        }
-
+                        {currentOrders.map((order) => (
+                          <tr key={order.orderId}>
+                            <td>{order.orderId}</td>
+                            <td>{order.orderDate}</td>
+                            <td>{order.payment.paymentMethod}</td>
+                            <td>{order.userId}</td>
+                          </tr>
+                        ))}
                       </tbody>
                     </table>
                   </div>
                 </div>
               </div>
-
+              {/* Pagination */}
+              <ReactPaginate
+                previousLabel={'Previous'}
+                nextLabel={'Next'}
+                breakLabel={'...'}
+                breakClassName={'page-item'}
+                breakLinkClassName={'page-link'}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={'pagination'}
+                activeClassName={'active'}
+                previousClassName={'page-item'}
+                nextClassName={'page-item'}
+                pageClassName={'page-item'}
+                previousLinkClassName={'page-link'}
+                nextLinkClassName={'page-link'}
+                pageLinkClassName={'page-link'}
+              />
             </div>
             {/* /.container-fluid */}
-
-
           </div>
         </div>
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default ListOrders
+export default ListOrders;
